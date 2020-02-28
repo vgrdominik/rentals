d583506d80c663b184a82c17029a41f628322c56
Valentí Gàmez <vgr.gamez@gmail.com>
2020-02-28 08:49:41 +0100

    Initial commit.

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <CtCard title="Novedades" width="600" class="mx-auto">
+    <v-row dense>
+      <v-spacer />
+      <CtBtn to="/registro">
+        Registro gratuito
+      </CtBtn>
+      <v-spacer />
+    </v-row>
+  </CtCard>
+</template>
+
+<script>
+export default {
+}
+</script>
